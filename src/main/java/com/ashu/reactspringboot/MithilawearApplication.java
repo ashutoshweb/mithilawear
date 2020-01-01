5e9457be719239d0229a7e233ba9956cc6b5762e
ashutoshdasjava <ashutoshid@yahoo.co.in>
2020-01-01 03:26:59 -0700

    Rename SpringBoot main class to Mithilawear

--- src/main/java/com/ashu/reactspringboot/MithilawearApplication.java
+++ src/main/java/com/ashu/reactspringboot/MithilawearApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactspringbootApplication {
+public class MithilawearApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReactspringbootApplication.class, args);
+        SpringApplication.run(MithilawearApplication.class, args);
     }
 
 }
